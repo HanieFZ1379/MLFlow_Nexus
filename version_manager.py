28e09664d36f8606def900b7fd859609c0df8545
Fatemeh Habibimoghaddam <121612207+FatemehHabibimoghaddam@users.noreply.github.com>
2024-12-30 05:30:07 -0800

    Update version_manager.py

--- version_manager.py
+++ version_manager.py
@@ -2,7 +2,7 @@ import mlflow
 from mlflow.tracking import MlflowClient
 
 # The tracking URI for the MLflow server
-mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_tracking_uri("http://37.152.191.193:8080/")
 print(f"Tracking URI: {mlflow.get_tracking_uri()}")
 
 # Initialize the MLflow client
